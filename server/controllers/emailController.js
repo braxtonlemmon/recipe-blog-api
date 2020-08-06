bb5bf590e330db0e98eaa792bd26f354ebe50fb7
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-08-06 15:09:27 +0200

    Add routes for newsletter.

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -1,5 +1,17 @@
 import Email from '../models/email';
 import { body, validationResult } from 'express-validator';
+import nodemailer from 'nodemailer';
+const he = require('he');
+
+const mailer = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.GMAIL_PASSWORD,
+  }
+});
 
 const indexEmails = (req, res, next) => {
   Email.find({ })
@@ -11,7 +23,7 @@ const indexEmails = (req, res, next) => {
   });
 }
 
-const createEmail = [
+const createAddress = [
   body('address', 'Email address is required').trim().isLength({ min: 1, max: 254 }),
   body('address').escape(),
 
@@ -33,7 +45,71 @@ const createEmail = [
   }
 ]
 
+const sendNewsletter = [
+  body('message', 'Message is required').trim().isLength({ min: 1 }),
+  body('subject', 'Subject is required.').trim().isLength({ min: 1 }),
+  body('message').escape(),
+  body('subject').escape(),
+
+  (req, res, next) => {
+    Email.find({ })
+    .exec((err, emails) => {
+      if (err) { return next(err); }
+      const addresses = emails.map(emailObj => emailObj.address);
+
+      mailer.verify(function(err, success) {
+        err ? console.log(err) : console.log('server is ready');
+      })
+      const message = he.decode(req.body.message);
+      const subject = he.decode(req.body.subject);
+      const myEmail = 'peelthegarlic@gmail.com';
+      mailer.sendMail(
+        {
+          from: myEmail,
+          to: addresses,
+          subject: subject,
+          text: message
+        },
+        function (err, info) {
+          if (err) return res.status(500).send(err);
+          res.json({ success: true });
+        }
+      )
+    })
+  }
+]
+
+const sendNewsletterTest = [
+  body("message", "Message is required").trim().isLength({ min: 1 }),
+  body("subject", "Subject is required.").trim().isLength({ min: 1 }),
+  body("message").escape(),
+  body("subject").escape(),
+
+  (req, res, next) => {
+    mailer.verify(function(err, success) {
+      err ? console.log(err) : console.log('server is ready');
+    })
+    const message = he.decode(req.body.message);
+    const subject = he.decode(req.body.subject);
+    const myEmail = 'peelthegarlic@gmail.com';
+    mailer.sendMail(
+      {
+        from: myEmail,
+        to: 'braxtonlemmon@gmail.com',
+        subject: subject,
+        text: message
+      },
+      function (err, info) {
+        if (err) return res.status(500).send(err);
+        res.json({ success: true })
+      }
+    )
+  }
+];
+
 export default {
   indexEmails,
-  createEmail
+  createAddress,
+  sendNewsletter,
+  sendNewsletterTest
 }
--- server/routes/emails.js
+++ server/routes/emails.js
@@ -7,12 +7,18 @@ const router = Router();
 // GET all email addresses
 router.get(
   "/",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
   emailController.indexEmails
 );
 
 // POST new email address to db
-router.post('/', emailController.createEmail);
+router.post('/', emailController.createAddress);
+
+// POST send a mass newsletter to all email addresses in db
+router.post('/newsletter', emailController.sendNewsletter);
+
+// POST send a test newsletter to myself
+router.post('/newsletterTest', emailController.sendNewsletterTest);
 
 export default router;
 
