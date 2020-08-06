9f0ea9dce2541a84e21d753d06444c1d696a2a88
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-08-06 10:17:09 +0200

    Require auth to GET email addresses.

--- server/routes/emails.js
+++ server/routes/emails.js
@@ -5,7 +5,11 @@ import passport from 'passport';
 const router = Router();
 
 // GET all email addresses
-router.get('/', emailController.indexEmails);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  emailController.indexEmails
+);
 
 // POST new email address to db
 router.post('/', emailController.createEmail);
