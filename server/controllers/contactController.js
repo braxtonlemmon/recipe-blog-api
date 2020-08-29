5a33532e6e33099f0716fa07f62d48059cb55c31
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-08-28 20:53:15 -0600

    Modify sendMail method in contactController with Namecheap auth instead of Gmail auth.

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -2,23 +2,20 @@ import { body, validationResult } from 'express-validator';
 import nodemailer from "nodemailer";
 const he = require('he');
 
-const contactAddress = "peelthegarlic@gmail.com";
+const contactAddress = "hello@peelthegarlic.com";
 
 const mailer = nodemailer.createTransport({
-  service: "gmail",
-  host: 'smtp.gmail.com',
+  service: "mail.privateemail.com",
+  host: 'mail.privateemail.com',
   port: 587,
   auth: {
-    user: process.env.GMAIL_ADDRESS,
-    pass: process.env.GMAIL_PASSWORD,
+    user: process.env.NAMECHEAP_EMAIL_ADDRESS,
+    pass: process.env.NAMECHEAP_EMAIL_PASSWORD,
   },
 });
 
 const sendMail = [
-  body('from', 'From is required').trim().isLength({ min: 1, max: 300 }),
-  body('message', 'Message is required').trim().isLength({ min: 1 }),
   body('email', 'Email is required.').trim().isLength({ min: 1 }),
-  body('from').escape(),
   body('subject').escape(),
   body('message').escape(),
   body('email').escape(),
@@ -32,15 +29,14 @@ const sendMail = [
       }
     })
     const message = he.decode(req.body.message);
-    const from = he.decode(req.body.from);
     const subject = he.decode(req.body.subject);
 
     mailer.sendMail(
       {
-        from: from,
+        from: `Peel the Garlic <${contactAddress}>`,
         to: contactAddress,
         subject: `PEEL THE GARLIC: ${subject || "No subject"}`,
-        text: `Message from ${from} (${req.body.email}): ${message || 'No message'}`
+        text: `Message from ${req.body.email}: ${message || 'No message'}`
       },
       function (err, info) {
         if (err) return res.status(500).send(err);
